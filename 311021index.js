559798cabb4be7b1a7c46f7a14dfaf963b3f55ba
Nadezhda Chusova <nadezda.chusova@yandex.ru>
2021-10-31 22:17:31 +0300

    ДЗ JS

--- 311021index.js
+++ 311021index.js
@@ -1,1 +1,29 @@
-console.log(typeof('Ivan'/4));
+/*Практика*/
++"22" * "3f"  // NaN
+2 + (7 + "$") //  '27$'
+"4px" - 2  // NaN
+8 / '3h' // NaN
+null + 1 // 1
+
+5 > 4 // true 
+"ананас" > "яблоко" // false  
+"2" > "12" // true 
+undefined == null  // true 
+undefined === null // false 
+null == "\n0\n" // false 
+null === +"\n0\n"  // false 
+"" + 1 + 0 // '10' 
+"" - 1 + 0 //  -1 
+true + false // 1
+6 / "3" // 2  
+"2" * "3"  // 6 
+4 + 5 + "px"  // "9px"   
+"$" + (4 + 5) //  '$9' 
+"4" - 2  // 2 
+"4px" - 2 // NaN 
+7 / 0  // Infinity 
+"-9" + 5 // "-95" 
+" -9 " - 5 // -14
+null + 1 // 1 
+undefined + 1 // NaN 
+" \t \n" - 2 // -2
